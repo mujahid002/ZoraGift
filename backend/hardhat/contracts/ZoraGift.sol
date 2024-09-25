04acc87d233eb1ceae106e03f823f64b59800305
Mujahid002 <mujahidshaik2002@gmail.com>
2024-09-25 17:53:03 +0530

    Sample ERC721 token

---
+++ backend/hardhat/contracts/ZoraGift.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @custom:security-contact mujahidshaik2002@gmail.com
+contract ZoraGift is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor()
+        ERC721("ZoraGift", "ZGC")
+        Ownable(_msgSender())
+    {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://sample/";
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable, ERC721Pausable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
