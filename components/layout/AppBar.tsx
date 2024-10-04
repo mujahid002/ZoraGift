842298d70ef976c1445ad09070b3cf9eb73b1809
mujahid002 <mujahidshaik2002@gmail.com>
2024-10-04 20:56:36 +0530

    updated

--- components/layout/AppBar.tsx
+++ components/layout/AppBar.tsx
@@ -5,17 +5,16 @@ import { Logo } from "@/components/layout/logo";
 
 export default function AppBar() {
   return (
-    <div className="fixed top-0 left-0 right-0 z-10 p-2 bg-white border-b">
-      <Menubar className="rounded-none border-none px-2 lg:px-4">
+    <div className="fixed top-0 left-0 right-0 z-10 bg-white border-b h-20 flex items-center">
+      <Menubar className="w-full rounded-none border-none px-6 lg:px-8">
         <MenubarMenu>
-          <div className=" lg:hidden"></div>
           <MenubarTrigger>
-            <Logo text="ZoraGift" />
+            <Logo text="ZoraGift" /> {/* Increase logo size */}
           </MenubarTrigger>
         </MenubarMenu>
         <div className="grow" />
-        <section className="my-12 mx-6 ">
-          <AccountMenu />
+        <section className="mr-6">
+          <AccountMenu /> {/* Increase account menu size */}
         </section>
       </Menubar>
     </div>
