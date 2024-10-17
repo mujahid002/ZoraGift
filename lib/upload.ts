b1f9b9c8f88f53014d56423d3a6c7e6df3b45d32
mujahid002 <mujahidshaik2002@gmail.com>
2024-10-17 15:41:08 +0530

    upload to IPFS

--- lib/upload.ts
+++ lib/upload.ts
@@ -1,8 +1,11 @@
 import { PinataSDK } from "pinata";
 
+import axios from 'axios';
+import FormData from 'form-data';
+
 export const Upload = async (data: any) => {
     const pinata = new PinataSDK({
-        pinataJwt: process.env.NEXT_PUBLIC_PINATA_SECRET_API_KEY,
+        pinataJwt: process.env.NEXT_PUBLIC_PINATA_JWT,
         pinataGateway: "white-underlying-coral-820.mypinata.cloud",
     });
 
@@ -12,5 +15,69 @@ export const Upload = async (data: any) => {
 
     return res;
 }
+
+interface UploadData {
+    file: File | Blob;
+    timestamp?: number;
+}
+
+export interface Metadata {
+    name: string;
+    description: string;
+    occasionType: string;
+    walletAddress: string;
+    amount: number;
+    timestamp: number;
+    isInstantGift: boolean;
+    createdBy?: string;
+    image: string | null;
+    content: {
+        mime: string;
+        uri: string | null;
+    };
+}
+
+
+export const handleUpload = async (metadata: Metadata): Promise<string> => {
+    try {
+        // Convert metadata to JSON string
+        const jsonString = JSON.stringify(metadata);
+
+        // Create a new Blob from the JSON string
+        const fileBlob = new Blob([jsonString], { type: 'application/json' });
+
+        // Create a new FormData instance
+        const formData = new FormData();
+
+        // Append the file to the form data
+        formData.append('file', fileBlob, 'metadata.json');
+
+        // Append Pinata metadata and options if needed
+        const pinataMetadata = {
+            name: `ZoraGift-${new Date().getTime().toString().slice(-1, -5)}`,
+        };
+        formData.append('pinataMetadata', JSON.stringify(pinataMetadata));
+
+        const pinataOptions = {
+            cidVersion: 1,
+        };
+        formData.append('pinataOptions', JSON.stringify(pinataOptions));
+
+        // Send the POST request to Pinata
+        const res = await axios.post('https://api.pinata.cloud/pinning/pinFileToIPFS', formData, {
+            maxContentLength: Infinity, // Prevent axios from throwing error due to large content
+            headers: {
+                Authorization: `Bearer ${process.env.NEXT_PUBLIC_PINATA_JWT}`, // Ensure this is server-side only
+            },
+        });
+
+        console.log(res.data);
+        return res.data.IpfsHash;
+    } catch (e) {
+        console.error(e);
+        throw new Error('Unable to upload metadata');
+    }
+};
+
 
 
